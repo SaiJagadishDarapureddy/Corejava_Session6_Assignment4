d8134392ea12cbaf784c205d3d76b24123e46727
SAI JAGADISH DARAPUREDDY <sai.jagadish.d@gmail.com>
2016-06-16 08:00:09 +0530

    Update StudentResult.java

--- StudentResult.java
+++ StudentResult.java
@@ -9,14 +9,14 @@ public class StudentResult {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        FirstSemester std1 = new FirstSemester("Shufol", 127, 149);// shufol roll and registration number
-        std1.setMarks(60,70);// set shufol marks
+        FirstSemester std1 = new FirstSemester("SAI JAGADISH", 408, 149);// SAI JAGADISH roll and registration number
+        std1.setMarks(60,70);// SAI JAGADISH marks
         System.out.println("\nEnter First Semester Student's Info:");
         std1.getSubject();
         std1.showResult();
         
-        SecondSemester std2 = new SecondSemester("Fakhrul", 227, 249);// Fakhrul roll and registration number
-        std2.setMarks(70,80);// set Fakhrul marks
+        SecondSemester std2 = new SecondSemester("VIJAY", 414, 249);// VIJAY roll and registration number
+        std2.setMarks(70,80);// set VIJAY marks
         System.out.println("\n\nEnter Second Semester Student's Info:");
         std2.getSubject();
         std2.showResult();
